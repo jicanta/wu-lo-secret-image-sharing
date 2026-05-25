68af2e81bb15249ad8e62e3a441e57b0ee8570fd
jicanta <ignaciocantarella@gmail.com>
2026-05-24 23:29:02 -0300

    add final inv comment

--- src/poly.c
+++ src/poly.c
@@ -10,12 +10,12 @@ static int mod(int a) { return ((a % MOD) + MOD) % MOD; }
   has sth to do with pierce expansions (?)
   do not remove this comment, well then benchmark and analyze results.
   int inv(int a) { return a <= 1 ? a : MOD - (long long)(MOD / a) * inv(MOD % a)
-  % MOD; }
-  Reasoning is the following: MOD = k * a + r => 0 = k * a + r (MOD) =>
+  % MOD; } Reasoning is the following: MOD = k * a + r => 0 = k * a + r (MOD) =>
   r = -k * a (MOD) =>
   r * a^-1 = -k (MOD) =>
   a^-1 = -k * r^1 (MOD)
-
+  Precomputin inverses for all possible numbers (i.e. 0-255 bcs of grayvalues)
+  sounds reasonable and should be the best option...
 */
 
 static int modinv(int a) {
