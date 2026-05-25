a4dea8f51b0e2b6816feb1a3c289217bb4145042
jicanta <ignaciocantarella@gmail.com>
2026-05-25 10:01:20 -0300

    precompute lagrange weights

--- src/poly.c
+++ src/poly.c
@@ -12,19 +12,10 @@ void poly_init(void) {
     inv_table[i] = MOD - (int)((long)(MOD / i) * inv_table[MOD % i] % MOD);
 }
 
-static int lagrange_eval_at_zero(const int* xs, const int* ys, int r) {
+static int lagrange_eval_at_zero(const int* vals, const int* weights, int r) {
   int result = 0;
-  for (int i = 0; i < r; i++) {
-    int num = 1, den = 1;
-    for (int j = 0; j < r; j++) {
-      if (j == i) continue;
-      num = (int)((long)num * mod(-xs[j]) % MOD);
-      den = (int)((long)den * mod(xs[i] - xs[j]) % MOD);
-    }
-    int term = (int)((long)mod(ys[i]) * num % MOD * inv_table[den] % MOD);
-    result = (result + term) % MOD;
-  }
-  return result;
+  for (int i = 0; i < r; i++) result += (int)((long)vals[i] * weights[i] % MOD);
+  return result % MOD;
 }
 
 int poly_eval(const int* coeffs, int r, int x) {
@@ -35,11 +26,22 @@ int poly_eval(const int* coeffs, int r, int x) {
 }
 
 void lagrange_recover(const int* xs, const int* ys, int r, int* coeffs_out) {
+  int weights[K_MAX];
+  for (int i = 0; i < r; i++) {
+    int num = 1, den = 1;
+    for (int j = 0; j < r; j++) {
+      if (j == i) continue;
+      num = (int)((long)num * mod(-xs[j]) % MOD);
+      den = (int)((long)den * mod(xs[i] - xs[j]) % MOD);
+    }
+    weights[i] = (int)((long)num * inv_table[den] % MOD);
+  }
+
   int vals[K_MAX];
   memcpy(vals, ys, r * sizeof(int));
 
   for (int k = 0; k < r; k++) {
-    coeffs_out[k] = lagrange_eval_at_zero(xs, vals, r);
+    coeffs_out[k] = lagrange_eval_at_zero(vals, weights, r);
     for (int i = 0; i < r; i++)
       vals[i] =
           (int)((long)mod(vals[i] - coeffs_out[k]) * inv_table[xs[i]] % MOD);
